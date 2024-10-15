412e91c2059a3571802747a6a7a6fbfd479294c0
halmel <j.h.nevim@seznam.cz>
2024-10-15 12:08:47 +0200

    first commit

---
+++ OsobaApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OsobaApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
